4ad30b250c313a5b8aaa98d3f4da380f6f8d1204
Aleksandr.Kashirin <saniok26@rambler.ru>
2022-02-08 00:29:30 +0200

    3-task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refFormData = document.querySelector('.form')
+
+refFormData.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  
+  let valueDelay = Number(event.currentTarget.elements.delay.value);
+  const valueStep = Number(event.currentTarget.elements.step.value);
+  const valueAmount = Number(event.currentTarget.elements.amount.value);
+
+
+  for (let position = 1; position <= valueAmount; position += 1) {
+    createPromise(position, valueDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    valueDelay += valueStep;
   }
+  refFormData.reset();
+} 
+
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+    
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay })
+      }
+    }, delay);
+  });
 }
+
+
